58e8e06a0fd9dad3938e018570ef414d4a023a04
thintalltoby <fagbemiadeshina@gmail.com>
2024-08-23 20:18:14 +0100

    eigth

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -76,10 +77,7 @@ WSGI_APPLICATION = 'blogproj.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
